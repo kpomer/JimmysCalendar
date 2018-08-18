a46320f8ee6d7275e7d99cff6bd5d130c6453ff6
Kevin Pomer <kpomer@ithaca.edu>
2018-08-18 02:02:28 -0400

    first commit on new branch

--- src/calendarEntry.java
+++ src/calendarEntry.java
@@ -2,5 +2,5 @@ public class calendarEntry {
 
     String title;
     int priority = 0; //Can be set to 0 (none), 1 (low), 2 (high) - default is 0
-    String note = ""; //User has option to add notes to their entry, but default is empty string
+    String note = ""; //User has option to add notes to their entry, but default is an empty string
 }

fb5a04bc5dd2c616a596aa788927388b5a6ecdbf
Kevin Pomer <kpomer@ithaca.edu>
2018-08-18 02:29:49 -0400

    created CommandLine class

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Jimmy's Calendar!");
         System.out.println("The purpose of this tool is to keep track of recurring events and deadlines.");
+
+        CommandLine userInterface = new CommandLine();
     }
 }
